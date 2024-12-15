809bce6f13e51058e059a6d5157e7d52c022efe6
wherewhere <qq2518766683@outlook.com>
2024-12-15 14:19:49 +0800

    添加项目

---
+++ index.js
@@ -0,0 +1,29 @@
+const fallback = require("./lib/fallback.js");
+
+/**
+ * @param {"jsdelivr" | "unpkg" | "cdnjs"} type 
+ */
+function getVendor(type) {
+  switch (type) {
+    case "jsdelivr":
+      return "https://cdn.jsdelivr.net/npm/css-vars-ponyfill";
+    case "unpkg":
+      return "https://unpkg.com/css-vars-ponyfill";
+    case "cdnjs":
+    default:
+      return "https://cdnjs.cloudflare.com/ajax/libs/css-vars-ponyfill/2.4.9/css-vars-ponyfill.min.js";
+  }
+}
+
+hexo.extend.generator.register(
+  "hexo-next-fallback",
+  () => {
+    return {
+      path: "js/third-party/next-fallback/fallback.js",
+      data: `${hexo.config.theme_config.motion?.enable ?? true ? `(${fallback.motion})();\n` : ''}(${fallback.css.toString().replace("${css-vars-ponyfill}", getVendor(hexo.config.theme_config.vendors?.plugins || "cdnjs"))})();`
+    };
+  });
+
+hexo.extend.injector.register(
+  "body_end",
+  '<script src="/js/third-party/next-fallback/fallback.js" defer></script>');
