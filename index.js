40681021ada03ccc3a6863e878e5492610ae0967
wherewhere <qq2518766683@outlook.com>
2024-12-29 15:01:14 +0800

    使用 Babel 编译 JS
    添加 polyfill

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { transformAsync } = require("@babel/core");
+const polyfill = require("./lib/polyfill.js")
 const fallback = require("./lib/fallback.js");
 
 /**
@@ -15,14 +17,67 @@ function getVendor(type) {
   }
 }
 
+/**
+ * @param {string} text
+ */
+async function babelAsync(text) {
+  const result = await transformAsync(text, { presets: [["@babel/preset-env"]], targets: { ie: "5" } });
+  return result.code;
+}
+
+/**
+ * @param {import("@types/hexo")} hexo 
+ */
+function createFallbackAsync(hexo) {
+  /** @type {string[]} */
+  const script = [];
+  const theme = hexo.config.theme_config;
+  if (theme.motion?.enable ?? true) {
+    script.push(`(${fallback.motion})();`);
+  }
+  if (theme.lazyload ?? false) {
+    script.push(`(${fallback.lazyload})();`);
+  }
+  script.push(`(${fallback.flex})();`);
+  script.push(`(${fallback.navbar})();`);
+  script.push(`(${fallback.scroll})();`);
+  script.push(`(${fallback.highlight})();`);
+  script.push(`(${fallback.css.toString().replace("${css-vars-ponyfill}", getVendor(theme.vendors?.plugins || "cdnjs"))})();`);
+  return babelAsync(script.join('\n'));
+}
+
+/**
+ * @param {import("@types/hexo")} hexo
+ * @param {string} path
+ * @param {() => Promise<string>} getTextAsync
+ */
+function renderGenerator(hexo, path, getTextAsync, engine = "js") {
+  return {
+    path: path,
+    data: async () => {
+      const text = await getTextAsync();
+      return await hexo.render.render({ text: text, engine: engine })
+    }
+  };
+}
+
+hexo.extend.generator.register(
+  "hexo-next-polyfill",
+  () => renderGenerator(
+    hexo,
+    "js/third-party/next-fallback/polyfill.js",
+    () => babelAsync(`(${polyfill})();`)));
+
+hexo.extend.injector.register(
+  "head_begin",
+  '<script src="/js/third-party/next-fallback/polyfill.js"></script>');
+
 hexo.extend.generator.register(
   "hexo-next-fallback",
-  () => {
-    return {
-      path: "js/third-party/next-fallback/fallback.js",
-      data: `${hexo.config.theme_config.motion?.enable ?? true ? `(${fallback.motion})();\n` : ''}${hexo.config.theme_config.lazyload ?? false ? `(${fallback.lazyload})();\n` : ''}(${fallback.flex})();\n(${fallback.navbar})();\n(${fallback.scroll})();\n(${fallback.highlight})();\n(${fallback.css.toString().replace("${css-vars-ponyfill}", getVendor(hexo.config.theme_config.vendors?.plugins || "cdnjs"))})();`
-    };
-  });
+  () => renderGenerator(
+    hexo,
+    "js/third-party/next-fallback/fallback.js",
+    () => createFallbackAsync(hexo)));
 
 hexo.extend.injector.register(
   "body_end",
